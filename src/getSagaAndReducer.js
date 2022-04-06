21c6a8bf4ed502c27d0a79f43c0d42ee1f8eb0b3
Nguyễn Duy Anh <anh.nguyen10@tiki.vn>
2022-04-07 01:56:58 +0700

    Polling effect

--- src/getSagaAndReducer.js
+++ src/getSagaAndReducer.js
@@ -1,5 +1,5 @@
 import * as R from 'ramda';
-import { put, throttle, takeLatest, takeEvery } from 'redux-saga/effects';
+import { put, take, throttle, takeLatest, takeEvery, delay } from 'redux-saga/effects';
 import * as sagaEffects from 'redux-saga/effects';
 import { persistReducer } from 'redux-persist';
 
@@ -8,9 +8,15 @@ function createRootSaga(sagas) {
   return function* rootSaga() {
     for (let i = 0; i < sagas.length; i++) {
       const { saga, namespace } = sagas[i];
-      const task = yield sagaEffects.fork(saga);
+      const _saga = Array.isArray(saga) ? saga[1] : saga;
+      const cancelKey = Array.isArray(saga) ? saga[0] : '';
+      const task = yield sagaEffects.fork(_saga);
       yield sagaEffects.fork(function* () {
-        yield sagaEffects.take(`${namespace}/@@CANCEL_EFFECTS`);
+        yield sagaEffects.take(
+          [`${namespace}/@@CANCEL_EFFECTS`, cancelKey && `${cancelKey}/@@CANCEL_EFFECT`].filter(
+            Boolean
+          )
+        );
         yield sagaEffects.cancel(task);
       });
     }
@@ -58,7 +64,13 @@ const applyModel = (storage, model, onError) => {
   return {
     namespace,
     reducer: R.isNil(_persistConfig) ? reducer : persistReducer(_persistConfig, reducer),
-    sagas: R.map((pair) => createEffect(pair, keys, onError), asyncReducerPairs),
+    sagas: R.map(
+      (pair) =>
+        isPolling(pair)
+          ? createEffectPolling(pair, keys, onError)
+          : createEffect(pair, keys, onError),
+      asyncReducerPairs
+    ),
   };
 };
 
@@ -114,6 +126,25 @@ function handlerWrapper(key, fn, _sagaEffects, onError) {
   };
 }
 
+function handlerWrapperPolling(key, fn, ms, _sagaEffects, onError) {
+  return function* (...args) {
+    yield take(key);
+    while (true) {
+      try {
+        yield put({ type: `${key}${NAMESPACE_SEP}@@start` });
+        yield fn(...[...args, _sagaEffects]);
+        yield put({ type: `${key}${NAMESPACE_SEP}@@end` });
+        yield delay(ms);
+      } catch (error) {
+        onError(error, {
+          key,
+          effectArgs: args,
+        });
+      }
+    }
+  };
+}
+const isPolling = (config) => R.equals('polling', R.path(['handler', 1, 'type'], config));
 function createEffect(config, availableKeys, onError) {
   const { key, namespace, handler } = config;
   const _key = getActionTypeWithCheck(namespace, key, availableKeys);
@@ -139,6 +170,21 @@ function createEffect(config, availableKeys, onError) {
   return function* effect() {
     yield takeEvery(_key, _sagaWithCatch);
   };
+}
+
+function createEffectPolling(config, availableKeys, onError) {
+  const { key, namespace, handler } = config;
+  const _key = getActionTypeWithCheck(namespace, key, availableKeys);
+  const ms = R.pathOr(0, [1, 'ms'], handler);
+  const _handler = R.head(R.filter(R.is(Function), [handler, R.head(handler)]));
+  const _sagaWithCatch = handlerWrapperPolling(
+    _key,
+    _handler,
+    ms,
+    createEffects(namespace, availableKeys, onError),
+    onError
+  );
+  return [_key, _sagaWithCatch];
 }
 
 const applyModels = ({ models, storage, onError }) => {
